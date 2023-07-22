c40bc6f9bb9cbc11cbe6bae34a3edf960b95a917
Michal Žamboch <zamboch.michal@seznam.cz>
2023-07-22 23:30:13 +0200

    Duplicate user control status.

--- text_works/DuplicateUserControl.xaml.cs
+++ text_works/DuplicateUserControl.xaml.cs
@@ -8,13 +8,17 @@ namespace text_works
     /// </summary>
     public partial class DuplicateUserControl : UserControl
     {
+        private readonly IStatus status;
+
         public DuplicateUserControl()
         {
+            status = ((MainWindow)Application.Current.MainWindow).status;
             InitializeComponent();
         }
         
         private void ClearButton_Click(object sender, RoutedEventArgs e)
         {
+            status.Set("Buffer cleared.");
             DuplicatesTextBlock.Text = string.Empty;
         }
 
@@ -26,6 +30,7 @@ namespace text_works
             {
                 DuplicatesTextBlock.Text += line + "\n";
             }
+            status.Set("Duplicates found.");
         }
     }
 }
